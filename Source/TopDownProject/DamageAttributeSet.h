308e114382add9baf1b855c652cd15bc0a1ce3e6
Tehnos <tehnos2013@gmail.com>
2024-10-05 17:57:39 +0300

    added attributesets

---
+++ Source/TopDownProject/DamageAttributeSet.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TopDownAttributeSet.h"
+#include "DamageAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWNPROJECT_API UDamageAttributeSet : public UTopDownAttributeSet
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData Damage;
+	ATTRIBUTE_ACCESSORS(UDamageAttributeSet, Damage);
+
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData Resistance;
+	ATTRIBUTE_ACCESSORS(UDamageAttributeSet, Resistance);
+
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData ManaPower;
+	ATTRIBUTE_ACCESSORS(UDamageAttributeSet, ManaPower);
+
+};
